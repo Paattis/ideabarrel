21254aee39ee720994f32d0dc34ce67d46673f3c
R <ramz9_97@hotmail.com>
2022-11-02 18:33:55 +0200

    react navigation installed

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import MainScreen from './screens/MainScreen';
 import AppContainer from './components/AppContainer';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
     <AppContainer>
-      <MainScreen />
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <MainScreen />
+        <StatusBar style="auto" />
+      </NavigationContainer>
     </AppContainer>
   );
 };
