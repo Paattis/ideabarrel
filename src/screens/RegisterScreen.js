8453b3d4ee2b766747cd78da9ea01620dac07138
R <ramz9_97@hotmail.com>
2022-11-11 19:36:13 +0200

    register screen validation

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -4,20 +4,18 @@ import { CustomInput, ScreenWrapper } from '../components';
 import { PropTypes } from 'prop-types';
 import { Button } from 'react-native-paper';
 import { useForm } from 'react-hook-form';
+import {
+  EMAIL_REGEX,
+  USERNAME_REGEX,
+  PASSWORD_REGEX,
+} from '../utils/variables';
 
 const RegisterScreen = ({ navigation }) => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { control, handleSubmit, watch } = useForm({ mode: 'onBlur' });
 
-  const signInScreen = (data) => {
-    console.log(data);
-    console.log(errors);
+  const signInScreen = () => navigation.navigate('SignIn');
 
-    navigation.navigate('SignIn');
-  };
+  const password = watch('password');
 
   return (
     <ScreenWrapper contentContainerStyle={styles.container} withScrollView>
@@ -25,33 +23,62 @@ const RegisterScreen = ({ navigation }) => {
         fieldName="email"
         label="Email"
         control={control}
-        rules={{ required: 'Email requited' }}
+        rules={{
+          required: 'Email required',
+          pattern: {
+            value: EMAIL_REGEX,
+            message: 'Email has to be valid.',
+          },
+        }}
       />
       <CustomInput
         fieldName="full_name"
         label="Full name"
         control={control}
-        rules={{ required: 'Full name requited' }}
+        rules={{
+          required: 'Full name required',
+          minLength: {
+            value: 3,
+            message: 'Full name must be at least 3 characters long',
+          },
+          maxLength: {
+            value: 20,
+            message: 'Full name can be maximum of 20 characters long',
+          },
+        }}
       />
       <CustomInput
         fieldName="username"
         label="Username"
         control={control}
-        rules={{ required: 'Username requited' }}
+        rules={{
+          required: 'Username required',
+          pattern: {
+            value: USERNAME_REGEX,
+            message: 'Username must be 2 - 15 characters long with no spaces',
+          },
+        }}
       />
       <CustomInput
         fieldName="password"
         label="Password"
-        secureTextEntry
         control={control}
-        rules={{ required: 'Password requited' }}
+        rules={{
+          required: 'Password required',
+          pattern: {
+            value: PASSWORD_REGEX,
+            message:
+              'Password must be at least 8 characters long with one uppercase character and a number',
+          },
+        }}
       />
       <CustomInput
         fieldName="confirm_password"
         label="Confirm password"
-        secureTextEntry
         control={control}
-        rules={{ required: 'Please confirm your password' }}
+        rules={{
+          validate: (value) => value === password || 'Password does not match',
+        }}
       />
 
       <Button onPress={handleSubmit(signInScreen)}>Sign Up</Button>
