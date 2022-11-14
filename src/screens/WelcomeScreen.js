9096aac083a8955a75e1ce46589606a8253f7ed7
R <ramz9_97@hotmail.com>
2022-11-15 00:05:00 +0200

    welcome screen ui and tests

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,27 +1,66 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { PropTypes } from 'prop-types';
-import { ScreenWrapper } from '../components';
-import { Button } from 'react-native-paper';
+import { Button, Divider, Text } from 'react-native-paper';
 
 const WelcomeScreen = ({ navigation }) => {
   const registerScreen = () => navigation.navigate('Sign Up');
   const signInScreen = () => navigation.navigate('Sign In');
 
   return (
-    <ScreenWrapper style={styles.container}>
-      <Text>WelcomeScreen</Text>
-
-      <Button onPress={registerScreen}>Sign Up</Button>
-      <Button onPress={signInScreen}>Sign In</Button>
-    </ScreenWrapper>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text variant="headlineMedium">Welcome</Text>
+        <Text variant="bodySmall" style={{ marginBottom: 29 }}>
+          IdeaBarrel allows you to share your work related ideas amongst your
+          colleagues quickly and efficiently!
+        </Text>
+        <Button
+          testID="sign_in"
+          style={styles.signInBtn}
+          mode="outlined"
+          onPress={signInScreen}
+        >
+          Sign In
+        </Button>
+        <View style={styles.divider}>
+          <Divider bold style={{ flex: 1 }} />
+          <View>
+            <Text style={styles.dividerText}>or</Text>
+          </View>
+          <Divider bold style={{ flex: 1 }} />
+        </View>
+        <Button testID="sign_up" mode="contained" onPress={registerScreen}>
+          Sign Up
+        </Button>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    backgroundColor: '#345BAC',
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    backgroundColor: 'white',
+    elevation: 50,
+    padding: 20,
+  },
+  divider: {
+    flexDirection: 'row',
     alignItems: 'center',
+    margin: 15,
+  },
+  dividerText: {
+    width: 50,
+    textAlign: 'center',
+    color: '#818181',
+  },
+  signInBtn: {
+    borderColor: '#345BAC',
   },
 });
 
