1f72c869a324b5492752889c36898a8114b667e5
R <ramz9_97@hotmail.com>
2022-11-15 01:29:31 +0200

    installed react-native-svg-transformer and refactored code

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,13 +1,11 @@
 import React, { StyleSheet } from 'react-native';
 import { useContext } from 'react';
-import { ScreenWrapper } from '../components';
-import { Svg, Path } from 'react-native-svg';
 import { Card } from 'react-native-paper';
 import { PropTypes } from 'prop-types';
 import { PreferencesContext } from '../contexts/PreferencesContext';
 import { CombinedDarkTheme, CombinedDefaultTheme } from '../theme';
 
-const CustomCard = ({ children, cardTitle, bgShape }) => {
+const CustomCard = ({ children, title }) => {
   const { isThemeDark } = useContext(PreferencesContext);
   const theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
 
@@ -16,41 +14,23 @@ const CustomCard = ({ children, cardTitle, bgShape }) => {
   };
 
   return (
-    <ScreenWrapper
-      contentContainerStyle={styles.container}
-      withScrollView
-      keyboardShouldPersistTaps="handled"
-    >
-      <Svg style={styles.bgShape}>
-        <Path d={bgShape} fill="#345BAC" />
-      </Svg>
-      <Card style={styles.card} elevation={4}>
-        <Card.Title titleStyle={cardTitleStyle} title={cardTitle} />
-        <Card.Content>{children}</Card.Content>
-      </Card>
-    </ScreenWrapper>
+    <Card style={styles.card} elevation={4}>
+      <Card.Title titleStyle={cardTitleStyle} title={title} />
+      <Card.Content>{children}</Card.Content>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   card: {
     width: '88%',
     borderRadius: 9,
-  },
-  bgShape: {
-    position: 'absolute',
   },
 });
 
 CustomCard.propTypes = {
   children: PropTypes.node,
-  cardTitle: PropTypes.string,
-  bgShape: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default CustomCard;
