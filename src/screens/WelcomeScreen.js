d7c9e2c38b5cdcd448956a42c8ff287576895bb1
R <ramz9_97@hotmail.com>
2022-11-15 02:08:37 +0200

    welcome screen ui done

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -2,46 +2,50 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { PropTypes } from 'prop-types';
 import { Button, Divider, Text } from 'react-native-paper';
+import WelcomeBG from '../../assets/svg/welcome-screen-bg';
 
 const WelcomeScreen = ({ navigation }) => {
   const registerScreen = () => navigation.navigate('Sign Up');
   const signInScreen = () => navigation.navigate('Sign In');
 
   return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <Text variant="headlineMedium">Welcome</Text>
-        <Text variant="bodySmall" style={{ marginBottom: 29 }}>
-          IdeaBarrel allows you to share your work related ideas amongst your
-          colleagues quickly and efficiently!
-        </Text>
-        <Button
-          testID="sign_in"
-          style={styles.signInBtn}
-          mode="outlined"
-          onPress={signInScreen}
-        >
-          Sign In
-        </Button>
-        <View style={styles.divider}>
-          <Divider bold style={{ flex: 1 }} />
-          <View>
-            <Text style={styles.dividerText}>or</Text>
+    <>
+      <View style={styles.container}>
+        <WelcomeBG />
+        <View style={styles.content}>
+          <Text variant="headlineMedium">Welcome</Text>
+          <Text variant="bodySmall" style={{ marginBottom: 29 }}>
+            IdeaBarrel allows you to share your work related ideas amongst your
+            colleagues quickly and efficiently!
+          </Text>
+          <Button
+            testID="sign_in"
+            style={styles.signInBtn}
+            mode="outlined"
+            onPress={signInScreen}
+          >
+            Sign In
+          </Button>
+          <View style={styles.divider}>
+            <Divider bold style={{ flex: 1 }} />
+            <View>
+              <Text style={styles.dividerText}>or</Text>
+            </View>
+            <Divider bold style={{ flex: 1 }} />
           </View>
-          <Divider bold style={{ flex: 1 }} />
+          <Button testID="sign_up" mode="contained" onPress={registerScreen}>
+            Sign Up
+          </Button>
         </View>
-        <Button testID="sign_up" mode="contained" onPress={registerScreen}>
-          Sign Up
-        </Button>
       </View>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#345BAC',
     flex: 1,
+    backgroundColor: '#345BAC',
     justifyContent: 'flex-end',
   },
   content: {
