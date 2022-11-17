47d62d7632d4ca7c906f3c79ac4c3b8644081c67
R <ramz9_97@hotmail.com>
2022-11-17 17:30:20 +0200

    poster details component done

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -7,8 +7,9 @@ import MainBG from '../../assets/svg/main-bg.svg';
 // mock data
 const data = [
   {
-    title: 'test1',
-    description: 'some desc',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
   {
     title: 'test2',
