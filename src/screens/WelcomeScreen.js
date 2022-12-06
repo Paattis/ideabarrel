94205cc7ce1d57b06817b0a7d39cf612957e0126
R <ramz9_97@hotmail.com>
2022-12-06 22:22:16 +0200

    loading indicator

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { PropTypes } from 'prop-types';
-import { Button, Divider, Text } from 'react-native-paper';
+import { ActivityIndicator, Button, Divider, Text } from 'react-native-paper';
 import { CombinedDarkTheme, CombinedDefaultTheme } from '../theme';
 import { PreferencesContext } from '../contexts/PreferencesContext';
 import { useAuth } from '../hooks';
@@ -11,8 +11,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import WelcomeBG from '../../assets/svg/welcome-screen-bg.svg';
 
 const WelcomeScreen = ({ navigation }) => {
-  const _registerScreen = () => navigation.navigate('Sign Up');
-  const _signInScreen = () => navigation.navigate('Sign In');
+  const [loading, setLoading] = useState();
 
   const { isThemeDark } = useContext(PreferencesContext);
   const { setUser, setSignedIn } = useContext(MainContext);
@@ -28,16 +27,22 @@ const WelcomeScreen = ({ navigation }) => {
     backgroundColor: theme.colors.background,
   };
 
+  const _registerScreen = () => navigation.navigate('Sign Up');
+  const _signInScreen = () => navigation.navigate('Sign In');
+
   const _authUser = async () => {
     const token = await AsyncStorage.getItem(ACCESS_TOKEN);
     if (!token) return;
 
     try {
+      setLoading(true);
       const user = await authUser(token);
       setUser(user);
       setSignedIn(true);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -45,39 +50,52 @@ const WelcomeScreen = ({ navigation }) => {
     _authUser();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <WelcomeBG />
-      <View style={[styles.content, contentColor]}>
-        <Text variant="headlineMedium">Welcome</Text>
-        <Text variant="bodySmall" style={{ marginBottom: 29 }}>
-          IdeaBarrel allows you to share your work related ideas amongst your
-          colleagues quickly and efficiently!
-        </Text>
-        <Button
-          testID="sign_in"
-          style={buttonStyle}
-          mode="outlined"
-          onPress={_signInScreen}
-        >
-          Sign In
-        </Button>
-        <View style={styles.divider}>
-          <Divider bold style={{ flex: 1 }} />
-          <View>
-            <Text style={styles.dividerText}>or</Text>
-          </View>
-          <Divider bold style={{ flex: 1 }} />
-        </View>
-        <Button testID="sign_up" mode="contained" onPress={_registerScreen}>
-          Sign Up
-        </Button>
+  if (loading)
+    return (
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator size="large" />
+        <Text style={{ margin: 10 }}>Authenticating...</Text>
       </View>
-    </View>
-  );
+    );
+  else
+    return (
+      <View style={styles.container}>
+        <WelcomeBG />
+        <View style={[styles.content, contentColor]}>
+          <Text variant="headlineMedium">Welcome</Text>
+          <Text variant="bodySmall" style={{ marginBottom: 29 }}>
+            IdeaBarrel allows you to share your work related ideas amongst your
+            colleagues quickly and efficiently!
+          </Text>
+          <Button
+            testID="sign_in"
+            style={buttonStyle}
+            mode="outlined"
+            onPress={_signInScreen}
+          >
+            Sign In
+          </Button>
+          <View style={styles.divider}>
+            <Divider bold style={{ flex: 1 }} />
+            <View>
+              <Text style={styles.dividerText}>or</Text>
+            </View>
+            <Divider bold style={{ flex: 1 }} />
+          </View>
+          <Button testID="sign_up" mode="contained" onPress={_registerScreen}>
+            Sign Up
+          </Button>
+        </View>
+      </View>
+    );
 };
 
 const styles = StyleSheet.create({
+  activityIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     backgroundColor: '#345BAC',
     justifyContent: 'flex-end',
